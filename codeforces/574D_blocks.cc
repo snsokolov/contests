1231691a176956e6f99357df5e7a08f89894789d
unknown <snsokolov@users.noreply.github.com>
2015-12-13 14:24:58 -0800

    Minor change

--- codeforces/574D_blocks.cc
+++ codeforces/574D_blocks.cc
@@ -3,7 +3,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Cppunit - C++ Unit testing TDD framework (github.com/cppunit/cppunit)
+// CPlusPlusUnit - C++ Unit testing TDD framework (github.com/cppunit/cppunit)
 class Cppunit { public:
     #define CHECK(a,b)  check<long long>(a, b, #a, #b, __FILE__, __LINE__, __FUNCTION__);
     #define CHECKT(a)   check<bool>(a, true, #a, "true", __FILE__, __LINE__, __FUNCTION__);
